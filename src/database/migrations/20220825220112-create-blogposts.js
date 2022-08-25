1fa6608e9d68f42b86f871a5c78b0b7f97e298f0
vitorbss12 <vitor_bss10@hotmail.com>
2022-08-25 19:40:29 -0300

    fix: fixing update to updated in posts migration

--- src/database/migrations/20220825220112-create-blogposts.js
+++ src/database/migrations/20220825220112-create-blogposts.js
@@ -31,7 +31,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE,
       },
-      update: {
+      updated: {
         allowNull: false,
         type: Sequelize.DATE,
       }
