4d4f465344654a8d4f9c2ee8b82c201721e17c17
vitorbss12 <vitor_bss10@hotmail.com>
2022-08-25 18:55:04 -0300

    fix: fixing config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -9,8 +9,7 @@ const suffix = {
 };
 
 const options = {
-  // host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  host: 'localhost',
+  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
   database: 
     `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment] || suffix.test}`,
