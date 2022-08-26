bfcd1ad6abfdc47dc45c83780a232198f4f3733b
vitorbss12 <vitor_bss10@hotmail.com>
2022-08-26 15:04:53 -0300

    feat: add unique key in email field to prevent duplicate email

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -2,14 +2,21 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     id: {
       type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true,
-  },
-  displayName: DataTypes.STRING,
-  email: DataTypes.STRING,
-  password: DataTypes.STRING,
-  image: DataTypes.STRING,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+    }, {
+      timestamps: false,
+      tableName: 'Users',
   }, {
-    timestamps: false,
-    tableName: 'Users',
+    indexes: [
+      {
+        unique: true,
+        fields: ['email'],
+      },
+    ],
   });
 
   // User.associate = (models) => {
