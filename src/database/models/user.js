6b883ff41cf7c4992d046c5e266366c7fc542f65
vitorbss12 <vitor_bss10@hotmail.com>
2022-08-26 12:00:24 -0300

    feat: creating model to User

---
+++ src/database/models/user.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true,
+  },
+  displayName: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+  image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'Users',
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.Post, { foreignKey: 'userId' });
+  };
+
+  return User;
+}
+
+
+
+/*
+id INT,
+displayName VARCHAR(255),
+email VARCHAR(255),
+password VARCHAR(255),
+image VARCHAR(255),
+Has Many Posts
+*/
